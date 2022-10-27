5985f7fd0cafd5ccd8258f096ecc547fa455f8be
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2022-10-27 02:57:31 -0300

    Comprobante de pago generado PDF

---
+++ admin/src/core/board/receipt.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from src.core.database import db
+
+
+class Receipt(db.Model):
+    __tablename__ = "receipts"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    member_full_name = db.Column(db.String(255))
+    total_amount = db.Column(db.String(255))
+    month_description = db.Column(db.String(255))
+
+    fee_id = db.Column(db.Integer, db.ForeignKey('fees.id'), nullable=False)
+
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now)
+    inserted_at = db.Column(db.Date, default=datetime.now())
