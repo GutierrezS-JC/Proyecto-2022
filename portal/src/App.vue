4f4605e1f6ab3ffff5f1cf6a2fd3299bfa9a32a8
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2022-11-17 19:29:57 -0300

    Cambio de carpeta raiz de proyecto

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="To Dare Is To Do" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
