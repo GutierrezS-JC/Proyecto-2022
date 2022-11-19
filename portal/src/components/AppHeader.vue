211c9700728c72bacc7fe13476dc254ff95e45f8
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2022-11-19 20:29:39 -0300

    Primera version de landing

---
+++ portal/src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Club Deportivo Villa Elisa</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Sobre nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Actividades</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sumate</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+        Insta Face Twitter
+      </span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "AppHeader"
+  }
+</script>
---
+++ portal/src/components/InformationMain.vue
@@ -0,0 +1,229 @@
+<template>
+  <div class="container-fluid" style="background-color: #fff5e5">
+    <div class="container pt-2 pt-md-5">
+      <div class="row">
+        <div class="col-12 col-lg-6 text-center d-block d-lg-none">
+          <img
+              alt="Vue logo"
+              class="logo"
+              src="@/assets/Escudo.png"
+              width="200"
+              height="200"
+          />
+        </div>
+        <div class="col-12 col-lg-6">
+          <h1 class="solidHeading">No es solo un club es una <span class="outlineHeading"> FAMILIA</span></h1>
+          <h2 class="h1" style="font-weight: bold">Sumate al <span style="color: #f6c169; letter-spacing: 8px;">DEPO</span></h2>
+          <p>Tenemos 99 años de historia. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <button type="button" class="btn btn-dark px-4">Sumate</button>
+          <img
+              alt="Vue logo"
+              class="logo_arrow"
+              src="@/assets/drawn_arrow.png"
+              width="85"
+              height="85"
+          />
+          <figure>
+            <blockquote>
+              <p class="text-muted">Club Deportivo de Villa Elisa (CDVE), apodado como Depo, es una de las
+                instituciones más importantes de la localidad.</p>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+              <cite title="Source Title">Diario Hoy</cite>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-12 col-lg-6 text-center d-none d-lg-block">
+          <img
+              alt="Vue logo"
+              class="logo"
+              src="@/assets/Escudo.png"
+              width="550"
+              height="550"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid pt-2 pt-md-5 ps-0" style="background-color: #fff5e5">
+      <div class="row">
+        <div class="col-12 col-lg-6 pe-0">
+          <img
+              alt="Vue logo"
+              class="logo"
+              src="@/assets/club_depo.webp"
+              width="100%"
+          />
+          <img
+              alt="Vue logo"
+              class="logo"
+              src="@/assets/knowyourhistory.jpg"
+              width="100%"
+          />
+        </div>
+        <div class="col-12 col-lg-6" style="background-color: #181818">
+          <div class="p-5 me-lg-5 text-white">
+            <h2 class="h5" style="color: #f6c169"><strong>Sobre nuestro club</strong></h2>
+            <h1 class="text-white" style="letter-spacing: 8px"><strong>Nuesta gran historia </strong> </h1>
+            <p>
+              El Club Deportivo Villa Elisa es una Asociación Civil sin fines de lucro, con más de un siglo de
+              antigüedad fomentando el deporte y la vida social y familiar. El 14 de enero de 1907 un grupo de amigos y vecinos de diferentes barrios de la localidad platense de Villa Elisa – todos de familias obreras fabriles, metalúrgicas, que trabajaban en el ferrocarril y la construcción- se unieron para formar un club en las cercanías de la plaza, a metros de la calle principal de la localidad y cerca de la estación ferroviaria.
+            </p>
+            <p>
+              Primero fue una modesta construcción de madera. La mano de obra solidaria de los asociados
+              fue delineando la ansiada pileta de natación. Con el correr del tiempo se logro ampliar las instalaciones,
+              comprando más terrenos del ala Norte, donde se construyó la cancha de papi fútbol que luego se amplió
+              para los deportes Básquet, Voley y Handball.
+            </p>
+            <p>
+              Hoy el club cuenta con una imponente instalación de los dos lados de la Avenida Centenario,
+              construida para celebrar el centenario de la patria a partir de 1910 y terminada 37 años más tarde,
+              donde sus socios pueden practicar Patín, Gimnasia artística, Taekwondo, y hasta ajedrez.
+            </p>
+            <p>
+              Además ya son míticos los carnavales y los bingos organizados por el club para seguir cumpliendo
+              con el principal objetivo del Club: que nuestros pibes tengan un lugar sano para desarrollarse y
+              los vecinos un espacio donde encontrarse.
+            </p>
+            <div class="row mt-5">
+              <div class="col-12 col-lg-4 text-center">
+                <strong style="color: #f6c169; font-size: 3em">1000</strong> <br> Socios
+              </div>
+              <div class="col-6 col-lg-4 text-center">
+                <strong style="color: #f6c169; font-size: 3em" >99</strong> <br> Años de historia
+              </div>
+              <div class="col-6 col-lg-4 text-center">
+                <strong style="color: #f6c169; font-size: 3em">X</strong> <br> Premios
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <div class="container-fluid" style="background-color: #ffd773">
+      <div class="row">
+        <div class="col-12 col-lg-4 pt-5" style="background-color: #fff2b8">
+          <div class="text-center mb-3 mt-3">
+            <img
+                alt="Vue logo"
+                class="logo"
+                src="@/assets/quote_w.png"
+                width="50"
+            />
+          </div>
+          <figure class="text-center p-3">
+            <blockquote class="blockquote solidQuote">
+              <p>Club Deportivo de Villa Elisa (CDVE), apodado como Depo, es una de las
+                instituciones más importantes de la localidad.</p>
+            </blockquote>
+            <figcaption class="blockquote-footer mt-2">
+              <cite title="Source Title" >Diario Hoy</cite>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-12 col-lg-4 pt-5" style="background-color: #eedb86">
+          <div class="text-center mb-3 mt-3">
+            <img
+                alt="Vue logo"
+                class="logo"
+                src="@/assets/quote_w.png"
+                width="50"
+            />
+          </div>
+          <figure class="text-center p-3">
+            <blockquote class="blockquote solidQuote">
+              <p>Sin lugar a dudas, el futuro del Depo es prometedor. En franco crecimiento, el CDVE dará que hablar
+                en los próximos años, no solo por su calidad deportiva, si no también, y especialmente, por la de su gente.</p>
+            </blockquote>
+            <figcaption class="blockquote-footer mt-2">
+              <cite title="Source Title">0221.com.ar</cite>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-12 col-lg-4 pt-5" style="background-color: #f6c169">
+          <div class="text-center mb-3 mt-3">
+            <img
+                alt="Vue logo"
+                class="logo"
+                src="@/assets/quote_w.png"
+                width="50"
+            />
+          </div>
+          <figure class="text-center p-3">
+            <blockquote class="blockquote solidQuote">
+              <p>El club Deporitvo Villa Elisa, con 96 años de vida, es una de las mas tradicionales
+                entidad del Norte [...] </p>
+            </blockquote>
+            <figcaption class="blockquote-footer mt-2">
+              <cite title="Source Title" >Diario El Dia</cite>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    </div>
+
+  <div class="container-fluid" style="background-color: #fff5e5">
+    <div class="container pt-4">
+      <h1 class="solidHeading">Actividades / Disciplinas</h1>
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <h1>Hola</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "InformationMain",
+
+  async created() {
+      try{
+        const response = await axios.get("https://admin-grupo26.proyecto2022.linti.unlp.edu.ar/api/club/disciplines")
+        console.log(response.data)
+      }
+      catch(err){
+        console.log(err)
+      }
+  }
+
+
+}
+</script>
+
+<style scoped>
+  .solidHeading{
+    font-family: 'Londrina Solid', cursive;
+    font-size:100px;
+  }
+
+  .solidSubheading{
+    font-family: 'Londrina Solid', cursive;
+    font-size:35px;
+  }
+
+  .solidQuote{
+     font-size:20px;
+  }
+
+  .outlineHeading{
+    font-size: 100px;
+    font-family: 'Londrina Shadow', cursive;
+  }
+
+  .logo_arrow{
+    margin-left: 25px;
+    transform: rotate(13deg);
+  }
+
+  .rotate{
+    transform: rotate(180deg);
+  }
+</style>
