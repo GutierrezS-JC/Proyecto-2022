05ed4e5b94030f45927e411197c40030e70a347c
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2022-11-20 01:33:14 -0300

    Agrego Footer de landing

--- portal/src/views/HomeView.vue
+++ portal/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
   import InformationMain from "@/components/InformationMain.vue";
   import AppHeader from "@/components/AppHeader.vue";
+  import AppFooter from "@/components/AppFooter.vue";
 </script>
 
 <template>
   <main>
     <AppHeader/>
     <InformationMain />
+    <AppFooter/>
   </main>
 </template>
